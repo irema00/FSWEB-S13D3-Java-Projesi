a4fa9df294a4e28be1fb1280e6785d4cc5d19df9
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-11 03:38:15 +0300

    implements Constructor Overloading and Chaining in Person Class

--- src/Person.java
+++ src/Person.java
@@ -10,8 +10,15 @@ public class Person {
         System.out.println("Person built");
     }
     public Person(String firstName, String lastName, int age){
+    this.firstName = firstName;
+    this.lastName=lastName;
+    this.age= age;
     }
     public Person(String firstName, String lastName, int age, String occupation, boolean isEmployed, double height){
+    this(firstName,lastName,age);
+    this.occupation =occupation;
+    this.isEmployed = isEmployed;
+    this.height = height;
 
     }
 }
