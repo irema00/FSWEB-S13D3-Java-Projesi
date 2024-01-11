a855fb7df15fa9fcb6884cfa0bf5e7be0dbb38b4
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-11 04:05:58 +0300

    adds test cases to main method for Wall class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Person Class Tests");
         Person person = new Person("Neo", "Tunga", 5);
         System.out.println("Firstname: " + person.getFirstName());
         System.out.println("LastName: " + person.getLastName());
         System.out.println("Age: " + person.getAge());
         System.out.println("Is Teen: " + person.isTeen());
+
+        System.out.println("Wall Class Tests");
+        Wall wall = new Wall(5, 4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
     }
 }
